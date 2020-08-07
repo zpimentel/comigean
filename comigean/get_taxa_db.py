ca6a136e2518403b4690764ee52e635fd87c548d
Zach <zach@Zach-MacBook-Pro.local>
2020-08-06 20:05:52 -0400

    Add support to download HMM db

--- comigean/get_taxa_db.py
+++ comigean/get_taxa_db.py
@@ -20,6 +20,19 @@ def unzip_db(outdir):
         raise Exception(f"The command {' '.join(cmd)} for database unpacking failed.")
 
 
+def hmmpress(outdir):
+    hmmpath = os.path.join(outdir, "Bacteria.hmm")
+    cmd = ['hmmpress', hmmpath]
+
+    try:
+        log_file = open("logfile", 'a')
+        hmmpress_command = subprocess.call(cmd, shell=False, stdout=log_file,
+                                           stderr=subprocess.STDOUT)
+        log_file.close()
+    except OSError as e:
+        raise Exception(f"The command ")
+
+
 def download_db(address, outdir, name):
     try:
         print(f"Downloading {name}:")
@@ -32,8 +45,11 @@ def download_db(address, outdir, name):
 def get_db(outdir):
     tax_ftp_address = "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip"
     refseq_ftp_address = "https://ftp.ncbi.nih.gov/genomes/refseq/assembly_summary_refseq.txt"
+    hmm_address = "https://raw.githubusercontent.com/AstrobioMike/GToTree/master/hmm_sets/Bacteria.hmm"
 
     download_db(tax_ftp_address, outdir, "NCBI Taxonomy Database")
     download_db(refseq_ftp_address, outdir, "NCBI RefSeq Summary")
+    download_db(hmm_address, outdir, "GToTree Marker Gene HMMs")
 
     unzip_db(outdir)
+    hmmpress(outdir)
